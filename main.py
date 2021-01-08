14eae3c2ae5444d75f5212841b96d25ceaad86ca
Amjad Alsharafi <amjadsharafi10@gmail.com>
2021-01-08 21:33:12 +0300

    DatasetGenerator: added the ability to run the generator with flask
    The flask server is empty, but just added the basics for now

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
 import sys
+from argparse import ArgumentParser
+
+from tqdm import tqdm
 
 from dataset_generator.generator import generate_pdfs_from_templates
 from dataset_generator.template import generate_latex_template
-from tqdm import tqdm
+from web import server as web_server
+
+
+def command_line_generation(out_dir):
+    templates = [
+        generate_latex_template("{num1} {operator1} {num2}{latin1}"),
+        generate_latex_template("{num1} {operator1} {num2}{latin1} {operator2} {num3}{latin1}^{{{num4}}} = 0"),
+        generate_latex_template("\\frac{{{num1}{latin1}}}{{{num2}}}"),
+    ]
+
+    progress = tqdm()
+    last_progress = 0
+
+    def updater(a, b):
+        nonlocal last_progress
+        if not last_progress:
+            # first time only
+            progress.reset(b)
+
+        progress.update(a - last_progress)
+        progress.display()
+        last_progress = a
+
+    generate_pdfs_from_templates(templates, out_dir, updater=updater, count_for_each=100)
+    progress.close()
+
 
 if __name__ == "__main__":
-    templates = [
-            generate_latex_template("{num1} {operator1} {num2}{latin1}"),
-            generate_latex_template("{num1} {operator1} {num2}{latin1} {operator2} {num3}{latin1}^{{{num4}}} = 0"),
-            generate_latex_template("\\frac{{{num1}{latin1}}}{{{num2}}}"),
-        ]
+    parser = ArgumentParser(description='Fyp1 dataset generator')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--outdir', type=str, help='run generation in command line')
+    group.add_argument('--web', action='store_true', help='run flask webserver frontend for dataset generation')
 
-    if len(sys.argv) < 2:
-        print(f"USAGE: {sys.argv[0]} <out_dir>")
+    args = parser.parse_args()
+
+    if args.web:
+        # run web server
+        web_server.run_server()
+    elif args.outdir:
+        command_line_generation(args.outdir)
     else:
-        progress = tqdm()
-        last_progress = 0
+        raise ValueError('invalid args')
 
-        def updater(a, b):
-            global last_progress
-            if not last_progress:
-                # first time only
-                progress.reset(b)
 
-            progress.update(a - last_progress)
-            progress.display()
-            last_progress = a
 
-        generate_pdfs_from_templates(templates, sys.argv[1], updater=updater, count_for_each=100)
-        progress.close()
