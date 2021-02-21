c03f7acc606f59bef7a3890909db57528e7b309c
Amjad Alsharafi <amjadsharafi10@gmail.com>
2021-02-21 09:56:36 +0300

    Classification: removed the generation of power_expr in the dataset
    Just removed the file from the metadata and images, but we still
    generate and split it.

--- classification_dataset_generator.py
+++ classification_dataset_generator.py
@@ -1,16 +1,18 @@
+import csv
 from argparse import ArgumentParser
+from os import path, mkdir, remove
+from string import digits, ascii_letters
+
+from PIL import Image
 from tqdm import tqdm
+
 from dataset_generator.generator import generate_single_from_template
 from segmenter.symbol_segmenter import segment_image
-from PIL import Image
-from string import digits, ascii_letters
-import csv
-from os import path, mkdir
+
 
 def generate_dataset(outdir):
     characters = list(digits + ascii_letters + "=-+")
     characters.extend(["\\Sigma", "\\pi"])
-
 
     if not path.isdir(outdir):
         # try to create directory
@@ -24,13 +26,12 @@ def generate_dataset(outdir):
     csv_writer = csv.writer(csv_file)
 
     counter = 0
-    
+
     def get_next_filename():
         nonlocal counter
         filename = f"{counter:04}"
         counter += 1
-        return filename 
-
+        return filename
 
     plain_template = "{0}"
     power_template = "{0}^{{{{{0}^{{{{{0}}}}}}}}}"
@@ -39,19 +40,18 @@ def generate_dataset(outdir):
         ch = characters[i]
 
         plain_filename = get_next_filename()
-        power_filename = get_next_filename()
+        power_filename = 'tmp_power_expr'
         plain_expr = generate_single_from_template(plain_template.format(ch),
-                outdir, plain_filename)
-        power_expr = generate_single_from_template(power_template.format(ch),
-                outdir, power_filename)
+                                                   outdir, plain_filename)
+        _power_expr = generate_single_from_template(power_template.format(ch),
+                                                   outdir, power_filename)
 
         csv_writer.writerow([plain_expr, plain_filename])
-        csv_writer.writerow([power_expr, power_filename])
 
         tmpimg = Image.open(path.join(outdir, power_filename + ".png"))
         crops = segment_image(tmpimg)
 
-        if  len(crops) != 3:
+        if len(crops) != 3:
             print(f"[ERROR] element {plain_expr} did not produce 3 crops, but produced {len(crops)}")
 
         for crop in crops:
@@ -59,6 +59,9 @@ def generate_dataset(outdir):
             filename = get_next_filename()
             cropped_img.save(path.join(outdir, filename + ".png"))
             csv_writer.writerow([plain_expr, filename])
+
+    remove(path.join(outdir, 'tmp_power_expr.png'))
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Fyp1 classfication dataset generator')
