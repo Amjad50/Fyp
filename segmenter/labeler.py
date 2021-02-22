bfd56197f1561448fdcb5a64f57e865a89937df5
Amjad Alsharafi <amjadsharafi10@gmail.com>
2021-02-22 10:26:41 +0300

    Segmentation: added labeler method, to label the segments
    Will be used later in frontend webapp

---
+++ segmenter/labeler.py
@@ -0,0 +1,22 @@
+from PIL import ImageColor, ImageDraw
+
+from .symbol_segmenter import segment_image
+
+
+def label_crops(img, crops=None):
+    if not crops:
+        crops = segment_image(img.convert('1'))
+    labeled_img = img.copy()
+    labeled_img = labeled_img.convert('RGB')
+    img_d = ImageDraw.Draw(labeled_img)
+
+    for i, crop in enumerate(crops):
+        l, t, r, d = crop
+        l -= 1
+        t -= 1
+
+        # goes through all hsl colors, evenly divided by the number of crops
+        color = ImageColor.getrgb(f'hsl({i / len(crops) * 360}, 100%, 50%)')
+        img_d.rectangle((l, t, r, d), outline=color)
+
+    return labeled_img
