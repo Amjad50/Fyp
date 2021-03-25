aa9c98a53a27fff7de90b637caac8f9bde2b6a0d
Amjad Alsharafi <amjadsharafi10@gmail.com>
2021-03-25 17:00:55 +0300

    Web-front: improved design of latex compiler and added query support
    Now, the last compiled expression is saved in the url. It can be saved
    or shared and once a url with query is opened, the front automatically
    sends a request to the backend to fetch the image.

--- web/static/js/latex_compiler.js
+++ web/static/js/latex_compiler.js
@@ -1,5 +1,8 @@
+let code_mirror_editor;
+let img;
+
 function initialize_code_mirror_editor() {
-    CodeMirror.fromTextArea(document.getElementById('latex_input'), {
+    code_mirror_editor = CodeMirror.fromTextArea(document.getElementById('latex_input'), {
         mode: "stex", theme: "dracula", lineNumbers: true, inMathMode: true,
     });
 }
@@ -20,7 +23,24 @@ function fix_brackets(latex_input) {
     return output;
 }
 
-let img;
+
+function compile_latex(latex_input) {
+
+    let url = '/api/v1/compile_latex?template=' + encodeURIComponent(latex_input);
+
+    if (!img) {
+        img = $('<img src="" alt="LaTeX compiled image display"/>');
+        $('#latex_output_container').append(img);
+    }
+    img.attr('src', url);
+}
+
+function compile_latex_and_update_input(latex_input) {
+    compile_latex(latex_input);
+
+    code_mirror_editor.getDoc().setValue(latex_input);
+}
+
 function add_latex_input_form_submit_handler() {
     let form = $('#latex_input_form');
 
@@ -32,14 +52,39 @@ function add_latex_input_form_submit_handler() {
 
         latex_input = fix_brackets(latex_input);
 
-        let url = '/api/v1/compile_latex?template=' + encodeURIComponent(latex_input);
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('template', latex_input);
 
-        if (!img) {
-            img = $('<img src="" alt="LaTeX compiled image display"/>');
-            $('#latex_output_container').append(img);
-        }
-        img.attr('src', url);
+        // window.location.search = urlParams.toString();
+        let new_url = window.location.href.split('?')[0];
+        new_url += "?" + urlParams.toString();
+
+        window.history.pushState({template: latex_input}, "", new_url);
+
+        compile_latex(latex_input);
     });
+}
+
+function add_history_pop_state_handler() {
+    window.onpopstate = function(event) {
+        if (event) {
+            if (event.state) {
+                compile_latex_and_update_input(event.state.template);
+            } else {
+                // no state, meaning that this it the start
+                compile_latex_and_update_input("");
+            }
+        }
+    }
+}
+
+function compile_params() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const template = urlParams.get('template');
+
+    if (template) {
+        compile_latex_and_update_input(template);
+    }
 }
 
 // Add the `CodeMirror` editor
@@ -47,3 +92,9 @@ initialize_code_mirror_editor();
 
 // attach handler to the form submit button
 add_latex_input_form_submit_handler();
+
+// history pushState
+add_history_pop_state_handler();
+
+// if there are params in the search query, then compile and display them
+compile_params();
