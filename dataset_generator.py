de0ec24509854f7d34c8865961f2801620e07ebb
Amjad Alsharafi <amjadsharafi10@gmail.com>
2021-04-10 16:09:48 +0300

    DatasetGeneration: added 2 more templates

--- dataset_generator.py
+++ dataset_generator.py
@@ -10,7 +10,9 @@ def command_line_generation(out_dir, count_for_each):
         "{num1} {operator1} {num2}{latin1}",
         "{num1} {operator1} {num2}{latin1} {operator2} {num3}{latin1}^{{{num4}}} = 0",
         "\\frac{{{num1}{latin1}}}{{{num2}}}",
-        "{latin1}_{latin2} =\\frac{{{latin4}^{{{digit1}}}}}{{{latin3}}}"
+        "{latin1}_{latin2} =\\frac{{{latin4}^{{{digit1}}}}}{{{latin3}}}",
+        "{latin1}({latin2}) = \\sum^{{{digit2}}}_{{{latin2}={{0}}}}{{{latin2}^{digit1}+\\frac{{{latin2}}}{{{latin2} + {num2}}}}}",
+        "e^{{{num1}}}+\\frac{{{latin1}^{digit1}+{latin2}^{digit2}}}{{{latin3}+{latin4}^{latin5}}}"
     ]
 
     progress = tqdm()
@@ -34,9 +36,8 @@ if __name__ == "__main__":
     parser = ArgumentParser(description='Fyp1 dataset generator')
     parser.add_argument('--outdir', '-o', type=str, required=True, help='run generation in command line')
     parser.add_argument('--count', '-c', type=int, action='store', default=20,
-            help='number of images to generate for each template [only for commandline] (default 20)')
+                        help='number of images to generate for each template [only for commandline] (default 20)')
 
     args = parser.parse_args()
 
     command_line_generation(args.outdir, args.count)
-
